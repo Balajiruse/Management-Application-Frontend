6b697c1523fa17bd5163a847447423284b1e4f74
Balajiruse <balaji.ruse@gmail.com>
2023-12-28 19:06:41 +0530

    task

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,57 @@
-
-
+import { useState } from "react";
+import './App.css';
+import ProductCard from "./ProductCard";
 function App() {
-  const [info, setinfo] = useState();
-  useEffect(()=>{
-    GetallUsers().then((data)=>{setinfo(data);} );
-   },[])
+  const data = [
+    {
+      productImage:
+        "https://www.skechers.in/on/demandware.static/-/Sites-skechers_india/default/dw17940ed5/images/large/195969748763-1.jpg",
+      productName: "Sketchers",
+    },
+    {
+      productImage:
+        "https://www.skechers.in/on/demandware.static/-/Sites-skechers_india/default/dw17940ed5/images/large/195969748763-1.jpg",
+      productName: "Nike",
+    },
+    {
+      productImage:
+        "https://www.skechers.in/on/demandware.static/-/Sites-skechers_india/default/dw17940ed5/images/large/195969748763-1.jpg",
+      productName: "Puma",
+    },
+    {
+      productImage:
+        "https://www.skechers.in/on/demandware.static/-/Sites-skechers_india/default/dw17940ed5/images/large/195969748763-1.jpg",
+      productName: "USP",
+    },
+  ];
+
+  const [product, setProduct] = useState([]);
+  const [inCart, setInCart] = useState(0);
+
+  // dummy dely
+  setTimeout(() => {
+    setProduct(data);
+  }, 1000);
+
   return (
-   <div className="App">
-    <Routes>
-      <Route exact path='/' element={<Mainpage/>}/>
-      <Route path='/adduser' element={<Addpage info={info} setinfo={setinfo}/>}/>
-      <Route path='/edituser/:id' element={<Editpage info={info} setinfo={setinfo}/>}/>
+    <div className="app">
+      <div className="cart-comp">
+        <h1>CART {inCart}</h1>
+      </div>
 
-
+      <div className="cart-area">
+        {product?.map((prod, idx) => (
+          <ProductCard
+            key={idx}
+            productImg={prod.productImage}
+            productName={prod.productName}
+            inCart={inCart}
+            setInCart={setInCart}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
+
+export default App;
